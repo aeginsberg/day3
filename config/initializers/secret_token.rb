2b551d60206e6a08c3bbcb2ae47f2293688170a7
Alex Ginsberg <aginsberg@wesleyan.edu>
2014-03-01 11:31:47 -0500

    start new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spacecats::Application.config.secret_key_base = 'b8b8b3a08776d6324d373daad127fb10cf89da09fa7f454a4be6202a2ee0c3bfd8d3ce870129877929d0c44aead2b2c1140eaff266a80bede7a5e11213591bf6'
